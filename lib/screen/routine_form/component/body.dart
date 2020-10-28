4eb4e739820b521d97d672baf718f710cadd6a03
latiefM <latief.kokom@gmail.com>
2020-10-28 16:27:27 +0700

    connect firebase

--- lib/screen/routine_form/component/body.dart
+++ lib/screen/routine_form/component/body.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:instro/constants.dart';
+
+import '../../../size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      height: SizeConfig.screenHeight,
+      width: SizeConfig.screenWidth,
+      color: kPrimaryColor,
+      child: ListView(children: <Widget>[
+
+        ListTile(
+          title: Text("GTG 1", style: TextStyle(fontSize: 16),),
+          leading: Icon(Icons.label),
+          
+          onTap: (){},
+        ),
+        ListTile(
+          title: Text("GTG 2", style: TextStyle(fontSize: 16),),
+          leading: Icon(Icons.label),
+          onTap: (){},
+        ),
+      ]),
     );
   }
 }
